c8e15fdadc1972e57c3d33e1715eac1cfdc0ab6e
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-03-01 13:21:09 +0000

    Added empty required functions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,9 @@
-console.log("connected")
+function runGame(){}
+function checkAnswer(){}
+function calculateCorrectAnswer(){}
+function incrementScore(){}
+function incrementWrongAnswer(){}
+function displayAdditionQuestion(){}
+function displaySubtractQuestion(){}
+function displayMultiplyQuestion(){}
+function displayDivisionQuestion(){}
